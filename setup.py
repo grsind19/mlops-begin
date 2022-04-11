cf71501ceb1e5b67a203f380e43ba266db63d09e
Seetharaman GR <grsind19@gmail.com>
2022-04-11 16:27:18 +0530

    changes for prediction

---
+++ setup.py
@@ -0,0 +1,12 @@
+from ensurepip import version
+from setuptools import setup, find_packages
+
+
+setup(
+  name = "src",
+  version = "0.0.1",
+  description="its a wine q",
+  author= "seetha",
+  packages=find_packages(),
+  license="MIT"
+)
